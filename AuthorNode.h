e3862304af569c17721d709e61fa58889d1b07e4
Alexander Furones <afurones43@gmail.com>
2019-04-19 10:40:42 -0400

    Add author node class file

---
+++ AuthorNode.h
@@ -0,0 +1,121 @@
+/*
+ *Alexander Furones Acosta
+ *S00756228
+ *COMP 315 - Introduction to data structures
+ *University Ana G Mendez
+ *Gurabo Campus
+ *School of Engineering
+
+ *Author linked node
+ *This class implements a singly linked node using templates
+ *to represent an author
+ *Each nodes the following attributes:
+	-fName: stores the first name of an author
+	-lName: stores the last name of an author
+	-next: a pointer to the next author in the list
+*/
+
+
+/*
+ *include necesarry libraries 
+ *and set name space
+*/
+
+#include <iostream>
+using namespace std;
+
+/*
+ *Class Declaration
+*/
+
+template <class T>
+class AuthorNode{
+public:
+	AuthorNode(); //Default constructor
+	AuthorNode(T, AuthorNode<T>*); //Special constructor
+	T getFName(); //get author's first name
+	T getLName(); //get author's last name
+	AuthorNode<T>* getNext(); // getNext Author
+	void setFName(T); //set author's first name
+	void setLName(T); //set author's last name
+	void setNext(IntSLLNode<T> *); //Set next Author
+private:
+	T fName; //Author's first name
+	T lName; //Author's last name
+	AuthorNode<T> *next; //Points to the next node in the list
+};
+
+
+/*
+ *Default constructor
+ *Creates a empty node
+ *next is zero by default
+*/
+template <class T>
+AuthorNode<T>::AuthorNode(){
+	next = 0;
+}
+
+/*
+ *Special constructor:
+ *Creates an author node with fName attribute of first
+ *and a lName attribute of last and next pointing to zero
+ *@params:
+	first: first name
+	last: last name
+	*n: pointer to the next author node
+*/
+template <class T>
+AuthorNode<T>::AuthorNode(T first, T last, AuthorNode<T> *n = 0){ 
+	fName = first;
+	LName = last;
+	next = n;
+}
+
+/*
+ *This function returns the first name of author
+*/
+template <class T>
+T AuthorNode<T>::getFName(){
+	return fName;
+}
+
+/*
+ *This method returns the last name of author
+*/
+template <class T>
+T AuthorNode<T>::getLName(){
+	return lName;
+}
+
+/*
+ *This method will return a pointer to the next author
+*/
+template <class T>
+AuthorNode<T> *AuthorNode<T>::getNext(){
+	return next;
+}
+
+/*
+ *This method will set first as the fName attribute
+*/
+template <class T>
+void AuthorNode<T>::setFNAME(T first){
+	fName = first;
+}
+
+/*
+ *This method will set last as the lName attribute
+*/
+template <class T>
+void AuthorNode<T>::setLNAME(T last){
+	lName = last;
+}
+
+/*
+ *This method sets next to point towards the next author node
+*/
+template <class T>
+void AuthorNode<T>::setNext(AuthorNode<T> *n = 0){
+	next = n;
+}
